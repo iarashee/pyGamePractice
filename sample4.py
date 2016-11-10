97e71bec8b5ddbca9b2599ad9936d60db02021ea
Ibrahim Rasheed <irasheed@Ibrahims-MacBook-Pro-2.local>
2016-11-10 13:57:46 -0500

    final: found pygame, edited sample4

--- sample4.py
+++ sample4.py
@@ -31,14 +31,14 @@ while not gameExit:
 	gameDisplay.fill(white)
 	#look up draw.rect()
 	pygame.draw.rect(gameDisplay, black, [400,300, 10, 100])
-	pygame.draw.rect(gameDisplay, red, [100,100, 50, 50])
+	pygame.draw.rect(gameDisplay, red, [500,250, 100, 100])
 
 	gameDisplay.fill(blue, rect=[200,200, 20,20])
 
 
 	gameDisplay.fill(blue, rect=[50,50, 20,20])
-	pygame.draw.circle(gameDisplay, red, (50,100), 20, 0)
-	pygame.draw.lines(gameDisplay, red, False, [(100,100), (150,200), (200,100)], 1)
+	pygame.draw.circle(gameDisplay, blue, (30,200), 20, 0)
+	pygame.draw.lines(gameDisplay, green, True, [(200,100), (150,200), (300,100)], 1)
 
 	pygame.display.update()		
 
